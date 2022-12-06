8c8d9acf1883bf47d77fecdb2220c3b99e1abd56
muckitymuck <andrewgoldstein@hotmail.com>
2022-12-06 13:35:35 -0600

    loading 404-  GOOD

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//import './index.css';
+import Theme from './components/Theme';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
+
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from '@apollo/client';
+import { ApolloLink } from 'apollo-boost';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import apolloLogger from 'apollo-link-logger';
+import { BrowserRouter } from 'react-router-dom';
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  }
+
+})
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    apolloLogger,
+    httpLink
+  ]),
+  cache: new InMemoryCache(),
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <Theme>
+        <App />
+      </Theme>
+    </ApolloProvider>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
