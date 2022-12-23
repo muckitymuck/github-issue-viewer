f79b55dc5a6ba1157ce4c330ed471e37a1150e84
muckitymuck <andrewgoldstein@hotmail.com>
2022-12-23 13:05:19 -0600

    build ready

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const IssueState = {
+    OPEN: 'OPEN',
+    CLOSED: 'CLOSED',
+};
--- src/routes/RepoPage.js
+++ src/routes/RepoPage.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Query } from 'react-apollo';
-// import queryString from 'query-string';
+import queryString from 'query-string';
 import gql from 'graphql-tag';
 
 import IssueList from '../components/IssueList';
-// import Header from '../components/LayoutHeader';
+import Header from '../components/LayoutHeader';
 import Content from '../components/LayoutContent';
-// import IssueListFilter from '../components/IssueListFilter';
-// import { IssueState } from '../constants';
-// import RepoHeader from '../components/RepoHeader';
+import IssueListFilter from '../components/IssueListFilter';
+import { IssueState } from '../constants';
+import RepoHeader from '../components/RepoHeader';
 
 const REPO_INFO_QUERY = gql`
 query Repo($owner: String!, $name: String!) {
@@ -50,7 +50,7 @@ class RepoPage extends Component {
 
         const { classes, match, location } = this.props;
         const { owner, name } = match.params;
-        // const { state = IssueState.OPEN } = queryString.parse(location.search);
+        const { state = IssueState.OPEN } = queryString.parse(location.search);
     
         return (
             <>
